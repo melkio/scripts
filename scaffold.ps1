72888f15af5e3b4950d2a206800b5c692978bd5b
Alessandro Melchiori <alessandro.melchiori@codiceplastico.com>
2016-04-07 16:05:15 +0200

    added first version of project/solution scaffold

---
+++ scaffold.ps1
@@ -0,0 +1,35 @@
+param([Parameter(Position = 0, Mandatory = 1)][string]$projectName),
+      [Parameter(Position = 1, Mandatory = 1)][string]$solutionName) 
+
+$projectPath = Join-Path "." -ChildPath $projectName
+New-Item $projectPath -Type directory
+
+$gitignore = Join-Path $projectPath -ChildPath ".gitignore"
+Invoke-WebRequest "https://raw.githubusercontent.com/github/gitignore/master/VisualStudio.gitignore" -OutFile $gitignore
+Add-Content $gitignore "`n# CUSTOM"
+Add-Content $gitignore "[Tt]ools"
+
+$srcPath = Join-Path $projectPath -ChildPath "src"
+New-Item $srcPath -Type directory
+$solutionPath = Join-Path $srcPath -ChildPath "$solutionName.sln"
+New-Item $solutionPath -Type file -Value "`nMicrosoft Visual Studio Solution File, Format Version 12.00`n
+# Visual Studio 14`n
+VisualStudioVersion = 14.0.23107.0`n
+MinimumVisualStudioVersion = 10.0.40219.1`n
+Global`n
+    GlobalSection(SolutionProperties) = preSolution`n
+        HideSolutionNode = FALSE`n
+    EndGlobalSection`n
+EndGlobal`n"
+
+$toolsPath = Join-Path $projectPath -ChildPath "tools"
+New-Item $toolsPath -Type directory
+
+$nugetPath = Join-Path $toolsPath -ChildPath "nuget"
+New-Item $nugetPath -Type directory
+$nuget = Join-Path $nugetPath -ChildPath "nuget.exe"
+Invoke-WebRequest "https://www.nuget.org/nuget.exe" -OutFile $nuget
+#&$nuget update -self
+
+&$nuget install psake -OutputDirectory $toolsPath -ExcludeVersion
+
