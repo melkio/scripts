c25dab0646fc70d7892cd71e72ec2ef1633dff46
Alessandro Melchiori <alessandro.melchiori@codiceplastico.com>
2016-04-07 16:08:38 +0200

    added setup script to configure a std environment (nuget, psake, nunit)

---
+++ setup.ps1
@@ -0,0 +1,16 @@
+$toolsPath = Join-Path . -ChildPath "tools"
+$toolsExists = Test-Path $toolsPath
+If ($toolsExists -eq $true) {
+    Remove-Item $toolsPath -Force -Recurse
+}
+
+New-Item $toolsPath -Type directory
+
+$nugetPath = Join-Path $toolsPath -ChildPath "nuget"
+New-Item $nugetPath -Type directory
+$nuget = Join-Path $nugetPath -ChildPath "nuget.exe"
+Invoke-WebRequest "https://www.nuget.org/nuget.exe" -OutFile $nuget
+&$nuget update -self
+
+&$nuget install psake -OutputDirectory $toolsPath -ExcludeVersion
+&$nuget install nunit.console -OutputDirectory $toolsPath -ExcludeVersion
